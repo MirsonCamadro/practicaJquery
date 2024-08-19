7f8cb4b918b13ea51616b5fdbee56538d45ea92b
MirsonCamadro <kztempus@gmail.com>
2024-08-19 19:22:10 -0400

    jquery render not working

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#button").click(function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+
+    $("#nameSummary").text(name);
+    $("#emailSummary").text(email);
+    $("#messageSummary").text(message);
+  });
+});
